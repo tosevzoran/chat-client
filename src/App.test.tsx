3c0387c4e37ed095fc17579bbf1fcb135644971b
zoki <tosevzoran@gmail.com>
2020-04-07 19:42:30 +0200

    feat(app): render the participant and message tabs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders participant and message tabs', () => {
+  const result = render(<App />);
+  const participantsTab = result.getByText(/Participants.*/i);
+  const messagesTab = result.getByText(/Messages.*/i);
+
+  expect(participantsTab).toBeInTheDocument();
+  expect(messagesTab).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import TabPanel from 'components/tab/TabPanel';
+import MessageList from 'components/messages/List';
+import ParticipantsList from 'components/participants/List';
 import './App.css';
+
+const messages = [
+  {
+    id: 1,
+    text:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    username: 'Lorem Ipsum',
+    timestamp: 23479547398,
+    isEdited: false,
+  },
+  {
+    id: 2,
+    text:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    username: 'Richard McClintock',
+    timestamp: 57498734587,
+    isEdited: false,
+  },
+  {
+    id: 3,
+    text:
+      'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.',
+    username: 'Finibus Bonorum',
+    timestamp: 57477373451,
+    isEdited: true,
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    username: 'Lorem Ipsum',
+  },
+  {
+    id: 2,
+    username: 'Richard McClintock',
+  },
+  {
+    id: 3,
+    username: 'Finibus Bonorum',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TabPanel defaultTabId="messages">
+        <TabPanel.TabHeader tabId="participants">
+          Participants
+        </TabPanel.TabHeader>
+        <TabPanel.TabHeader tabId="messages">Messages</TabPanel.TabHeader>
+        <div>
+          <TabPanel.TabContent tabId="participants">
+            <ParticipantsList participants={users} />
+          </TabPanel.TabContent>
+          <TabPanel.TabContent tabId="messages">
+            <MessageList messages={messages} />
+          </TabPanel.TabContent>
+        </div>
+      </TabPanel>
     </div>
   );
 }
