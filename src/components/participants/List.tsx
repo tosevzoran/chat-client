9208fae8c93ba80e2d900f853bcb5371765f9b8e
zoki <tosevzoran@gmail.com>
2020-04-07 22:04:35 +0200

    style(participants): add style to the participant list

--- src/components/participants/List.tsx
+++ src/components/participants/List.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface User {
   id: number | string;
@@ -9,17 +10,30 @@ interface ParticipantListProps {
   participants: User[];
 }
 
+const StyledList = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  overflow-y: auto;
+  height: 100%;
+`;
+
+const StyledListItem = styled.li`
+  padding: 1rem 2rem;
+  border-bottom: 0.0625rem solid #cccccc;
+`;
+
 const List: React.FC<ParticipantListProps> = ({ participants }) => {
   if (!participants.length) {
     return <p>This thread has no users</p>;
   }
 
   return (
-    <ul>
+    <StyledList>
       {participants.map(({ id, username }) => (
-        <li key={id}>{username}</li>
+        <StyledListItem key={id}>{username}</StyledListItem>
       ))}
-    </ul>
+    </StyledList>
   );
 };
 
