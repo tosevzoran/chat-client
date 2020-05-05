c82df3c941172cb2e6104032f9026aa31222de64
zoki <tosevzoran@gmail.com>
2020-05-05 11:07:17 +0200

    fix(time-formating): properly handle the case when timestamp is not passed

--- src/helper/format.ts
+++ src/helper/format.ts
@@ -1,4 +1,4 @@
 import format from 'date-fns/format';
 
 export const timestampToString = (timestamp?: number) =>
-  format(new Date(timestamp ? timestamp * 1000 : 'now'), 'H:mm');
+  format(timestamp ? new Date(timestamp * 1000) : new Date(), 'H:mm');
